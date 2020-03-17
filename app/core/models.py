bb945239bc9db56403684b6b679d24969d9a9238
Daniel <danieljust2395@gmail.com>
2020-03-17 06:08:14 -0500

    test commit with flake8 installed

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
+# Test commit
+
 
 class UserManager(BaseUserManager):
 
